530ee402a0782470531d2e1ee795ca75c21c7beb
blissful-y0 <blissful.y0@gmail.com>
2021-08-31 00:17:50 +0900

    Update: nomad-coder day 7 challenge done

--- javascript-nomadcoder-challenge/day7-challenge/src/index.js
+++ javascript-nomadcoder-challenge/day7-challenge/src/index.js
@@ -3,13 +3,32 @@ const selectedNumber = document.querySelector(".selectedNumber");
 const submittedNumber = document.querySelector(".submittedNumber");
 const selectedValue = document.querySelector(".selectedValue");
 const machineValue = document.querySelector(".machineValue");
+const result = document.querySelector(".result");
+const resultBoard = document.querySelector(".resultBoard");
 
 const onClickPlayButton = (event) => {
   event.preventDefault();
-  let maxRangeValue = selectedNumber.value;
-  let userNumber = submittedNumber.value;
+
+  resultBoard.style.visibility = "visible";
+
+  let maxRangeValue = parseInt(selectedNumber.value);
+  let userNumber = parseInt(submittedNumber.value);
   let randomNumber = Math.floor(Math.random() * maxRangeValue);
-  console.log(userNumber);
+
+  selectedValue.textContent = userNumber;
+  machineValue.textContent = randomNumber;
+
+  if (userNumber > maxRangeValue)
+    return (result.textContent =
+      "Your guess number is bigger than maximum number range");
+
+  if (!userNumber || !maxRangeValue) {
+    return (result.textContent = "You have not entered the value");
+  } else {
+    randomNumber === userNumber
+      ? (result.textContent = "You win!")
+      : (result.textContent = "You lost!");
+  }
 };
 
 button.addEventListener("click", onClickPlayButton);
